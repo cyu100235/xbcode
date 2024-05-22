e4a454f2c5b8c290bd865bd35568b67218c4752d
楚羽幽 <cy958416459@qq.com>
2024-05-22 13:43:44 +0800

    默认情况下开启调试模式

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 use support\Request;
 
 return [
-    'debug' => xbEnv('APP_DEBUG', false),
+    'debug' => xbEnv('APP_DEBUG', true),
     'error_reporting' => E_ALL,
     'default_timezone' => 'Asia/Shanghai',
     'request_class' => Request::class,
