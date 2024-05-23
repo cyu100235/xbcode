d659f94df422c4c11cfa2c2c0a5ec47197e3fe60
楚羽幽 <cy958416459@qq.com>
2024-05-23 19:25:52 +0800

    增加安装时函数检测

--- app/common/utils/EnvironmentUtil.php
+++ app/common/utils/EnvironmentUtil.php
@@ -55,6 +55,132 @@ class EnvironmentUtil
             'status'    => false,
             'value'     => 'fail'
         ],
+        [
+            'title'     => 'stream_socket_server',
+            'name'      => 'stream_socket_server',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'stream_socket_client',
+            'name'      => 'stream_socket_client',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'pcntl_signal_dispatch',
+            'name'      => 'pcntl_signal_dispatch',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'pcntl_signal',
+            'name'      => 'pcntl_signal',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'pcntl_alarm',
+            'name'      => 'pcntl_alarm',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'pcntl_fork',
+            'name'      => 'pcntl_fork',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_getuid',
+            'name'      => 'posix_getuid',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_getpwuid',
+            'name'      => 'posix_getpwuid',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_kill',
+            'name'      => 'posix_kill',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_setsid',
+            'name'      => 'posix_setsid',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_getpid',
+            'name'      => 'posix_getpid',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_getpwnam',
+            'name'      => 'posix_getpwnam',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_getgrnam',
+            'name'      => 'posix_getgrnam',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_getgid',
+            'name'      => 'posix_getgid',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_setgid',
+            'name'      => 'posix_setgid',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_initgroups',
+            'name'      => 'posix_initgroups',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_setuid',
+            'name'      => 'posix_setuid',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'posix_isatty',
+            'name'      => 'posix_isatty',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'proc_open',
+            'name'      => 'proc_open',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'proc_get_status',
+            'name'      => 'proc_get_status',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
+        [
+            'title'     => 'proc_close',
+            'name'      => 'proc_close',
+            'status'    => false,
+            'value'     => 'fail'
+        ],
     ];
 
     /**
@@ -102,6 +228,13 @@ class EnvironmentUtil
         [
             'title'  => 'gd',
             'name'   => 'gd',
+            'type'   => 'extra',
+            'status' => false,
+            'value'  => 'fail'
+        ],
+        [
+            'title'  => 'exif',
+            'name'   => 'exif',
             'type'   => 'extra',
             'status' => false,
             'value'  => 'fail'
