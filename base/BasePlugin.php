7ac7907a12aa86bb881b5c405806ace156892600
楚羽幽 <cy958416459@qq.com>
2025-02-21 13:23:44 +0800

    - 重构插件基类并引入安装、更新和卸载插件化机制 - 移除了 BasePlugin 中的冗余代码和未使用的引用 - 引入了 InstallTrait、UpdateTrait 和 UnInstallTrait 插件化机制 - 修改了 install、update 和 uninstall 方法的签名，允许传递额外的 context 参数 - 更新了 config/view.php，将视图处理器改为 ThinkPHP - 删除了未使用的 redirect.vue 相关代码

--- base/BasePlugin.php
+++ base/BasePlugin.php
@@ -1,12 +1,9 @@
 <?php
 namespace plugin\xbCode\base;
 
-use Exception;
-use think\facade\Db;
-use plugin\xbCode\api\Menus;
-use plugin\xbCode\api\Mysql;
-use plugin\xbDict\api\DictApi;
-use plugin\xbCode\api\Packages;
+use plugin\xbCode\base\plugin\UpdateTrait;
+use plugin\xbCode\base\plugin\InstallTrait;
+use plugin\xbCode\base\plugin\UnInstallTrait;
 
 /**
  * 插件基类
@@ -15,6 +12,13 @@ use plugin\xbCode\api\Packages;
  */
 abstract class BasePlugin
 {
+    // 插件安装
+    use InstallTrait;
+    // 插件更新
+    use UpdateTrait;
+    // 插件卸载
+    use UnInstallTrait;
+
     /**
      * 安装前置
      * @param string $version 版本名称
@@ -24,16 +28,6 @@ abstract class BasePlugin
      */
     public static function installBefore(string $version = '')
     {
-        // 获取插件名称
-        $name = self::getCallPluginName();
-        try {
-            // 检查依赖与插件
-            Packages::checked($name);
-        } catch (\Throwable $th) {
-            if ($th->getCode() != 1) {
-                throw $th;
-            }
-        }
     }
 
     /**
@@ -44,11 +38,7 @@ abstract class BasePlugin
      * @copyright 贵州小白基地网络科技有限公司
      * @author 楚羽幽 cy958416459@qq.com
      */
-    public static function install(string $version = '')
-    {
-        // 返回数据给安装后
-        return [];
-    }
+    abstract public static function install(string $version = '', mixed $context = null);
 
     /**
      * 安装后置
@@ -58,7 +48,7 @@ abstract class BasePlugin
      * @copyright 贵州小白基地网络科技有限公司
      * @author 楚羽幽 cy958416459@qq.com
      */
-    public static function installAfter(string $version = '')
+    public static function installAfter(string $version = '', mixed $context = null)
     {
         // 可以自己实现安装之后的业务逻辑...
     }
@@ -71,7 +61,7 @@ abstract class BasePlugin
      * @copyright 贵州小白基地网络科技有限公司
      * @author 楚羽幽 cy958416459@qq.com
      */
-    public static function updateBefore(string $localVersion = '')
+    public static function updateBefore(string $localVersion = '', string $toVersion = '')
     {
         // 返回数据给更新
         return [];
@@ -86,11 +76,7 @@ abstract class BasePlugin
      * @copyright 贵州小白基地网络科技有限公司
      * @author 楚羽幽 cy958416459@qq.com
      */
-    public static function update(string $localVersion = '', string $toVersion = '')
-    {
-        // 返回数据给更新后
-        return [];
-    }
+    abstract public static function update(string $localVersion = '', string $toVersion = '', mixed $context = null);
 
     /**
      * 更新后置
@@ -101,7 +87,7 @@ abstract class BasePlugin
      * @copyright 贵州小白基地网络科技有限公司
      * @author 楚羽幽 cy958416459@qq.com
      */
-    public static function updateAfter(string $localVersion = '', string $toVersion = '')
+    public static function updateAfter(string $localVersion = '', string $toVersion = '', mixed $context = null)
     {
     }
 
@@ -126,11 +112,7 @@ abstract class BasePlugin
      * @copyright 贵州小白基地网络科技有限公司
      * @author 楚羽幽 cy958416459@qq.com
      */
-    public static function uninstall(string $localVersion = '')
-    {
-        // 返回数据给卸载后
-        return [];
-    }
+    abstract public static function uninstall(string $localVersion = '', mixed $context = null);
 
     /**
      * 卸载后置
@@ -140,100 +122,7 @@ abstract class BasePlugin
      * @copyright 贵州小白基地网络科技有限公司
      * @author 楚羽幽 cy958416459@qq.com
      */
-    public static function uninstallAfter(string $localVersion = '')
+    public static function uninstallAfter(string $localVersion = '', mixed $context = null)
     {
-    }
-
-    /**
-     * 安装数据库
-     * @return void
-     * @copyright 贵州小白基地网络科技有限公司
-     * @author 楚羽幽 cy958416459@qq.com
-     */
-    protected static function installSql()
-    {
-        // 数据库信息
-        $database = Db::getConfig();
-        $prefix = $database['connections']['mysql']['prefix'] ?? 'xb_';
-        // 替换表前缀
-        $oldPrefix = ['`xb_', '`php_', '`__PREFIX__'];
-        $newPrefix = "`{$prefix}";
-        // 获取插件名称
-        $plugin = self::getCallPluginName();
-        // SQL文件地址
-        $sqlFile = base_path() . "/plugin/{$plugin}/install.sql";
-        // 安装数据库
-        Mysql::importSql($sqlFile, $oldPrefix, $newPrefix);
-    }
-
-    /**
-     * 安装菜单
-     * @return void
-     * @copyright 贵州小白基地网络科技有限公司
-     * @author 楚羽幽 cy958416459@qq.com
-     */
-    protected static function installMenus()
-    {
-        // 获取插件名称
-        $plugin = self::getCallPluginName();
-        // 获取菜单文件
-        $menuFile = base_path() . "/plugin/{$plugin}/config/menu.php";
-        if (!file_exists($menuFile)) {
-            return;
-        }
-        // 获取菜单数据
-        $menus = include $menuFile;
-        // 检测菜单数据
-        if (empty($menus)) {
-            return;
-        }
-        // 开始安装菜单
-        Menus::install($menus, $plugin);
-    }
-    
-    /**
-     * 安装字典
-     * @return void
-     * @copyright 贵州小白基地网络科技有限公司
-     * @author 楚羽幽 cy958416459@qq.com
-     */
-    protected static function installDict()
-    {
-        // 获取插件名称
-        $plugin = self::getCallPluginName();
-        // 获取字典文件
-        $dictFile = base_path() . "/plugin/{$plugin}/config/dict.php";
-        if (!file_exists($dictFile)) {
-            return;
-        }
-        // 获取菜单数据
-        $dict = include $dictFile;
-        // 检测菜单数据
-        if (empty($dict)){
-            return;
-        }
-        // 开始安装字典数据
-        DictApi::installAction()->install($plugin);
-    }
-
-    /**
-     * 获取调用类插件名称
-     * @throws Exception
-     * @return string
-     * @copyright 贵州小白基地网络科技有限公司
-     * @author 楚羽幽 cy958416459@qq.com
-     */
-    public static function getCallPluginName()
-    {
-        // 获取调用类命名空间
-        $callClass = get_called_class();
-        // 获取插件名称
-        $parseClass = explode('\\', $callClass);
-        // 检测是否正确获取到插件名称
-        if (empty($parseClass[1]) || $parseClass[0] != 'plugin') {
-            throw new Exception('插件名称获取失败');
-        }
-        // 返回插件名称
-        return $parseClass[1];
     }
 }
---
+++ base/plugin/InstallTrait.php
@@ -0,0 +1,109 @@
+<?php
+namespace plugin\xbCode\base\plugin;
+
+use Exception;
+use think\facade\Db;
+use plugin\xbCode\api\Menus;
+use plugin\xbCode\api\Mysql;
+use plugin\xbDict\api\DictApi;
+
+/**
+ * 安装插件方法
+ * @copyright 贵州小白基地网络科技有限公司
+ * @author 楚羽幽 cy958416459@qq.com
+ */
+trait InstallTrait
+{
+    /**
+     * 安装数据库
+     * @return void
+     * @copyright 贵州小白基地网络科技有限公司
+     * @author 楚羽幽 cy958416459@qq.com
+     */
+    protected static function installSql()
+    {
+        // 数据库信息
+        $database = Db::getConfig();
+        $prefix = $database['connections']['mysql']['prefix'] ?? 'xb_';
+        // 替换表前缀
+        $oldPrefix = ['`xb_', '`php_', '`__PREFIX__'];
+        $newPrefix = "`{$prefix}";
+        // 获取插件名称
+        $plugin = self::getCallPluginName();
+        // SQL文件地址
+        $sqlFile = base_path() . "/plugin/{$plugin}/install.sql";
+        // 安装数据库
+        Mysql::importSql($sqlFile, $oldPrefix, $newPrefix);
+    }
+
+    /**
+     * 安装菜单
+     * @return void
+     * @copyright 贵州小白基地网络科技有限公司
+     * @author 楚羽幽 cy958416459@qq.com
+     */
+    protected static function installMenus()
+    {
+        // 获取插件名称
+        $plugin = self::getCallPluginName();
+        // 获取菜单文件
+        $menuFile = base_path() . "/plugin/{$plugin}/config/menu.php";
+        if (!file_exists($menuFile)) {
+            return;
+        }
+        // 获取菜单数据
+        $menus = include $menuFile;
+        // 检测菜单数据
+        if (empty($menus)) {
+            return;
+        }
+        // 开始安装菜单
+        Menus::install($menus, $plugin);
+    }
+    
+    /**
+     * 安装字典
+     * @return void
+     * @copyright 贵州小白基地网络科技有限公司
+     * @author 楚羽幽 cy958416459@qq.com
+     */
+    protected static function installDict()
+    {
+        // 获取插件名称
+        $plugin = self::getCallPluginName();
+        // 获取字典文件
+        $dictFile = base_path() . "/plugin/{$plugin}/config/dict.php";
+        if (!file_exists($dictFile)) {
+            return;
+        }
+        // 获取菜单数据
+        $dict = include $dictFile;
+        // 检测菜单数据
+        if (empty($dict)){
+            return;
+        }
+        // 开始安装字典数据
+        DictApi::installAction()->install($plugin);
+    }
+
+    /**
+     * 获取调用类插件名称
+     * @throws Exception
+     * @return string
+     * @copyright 贵州小白基地网络科技有限公司
+     * @author 楚羽幽 cy958416459@qq.com
+     */
+    public static function getCallPluginName()
+    {
+        // 获取调用类命名空间
+        $callClass = get_called_class();
+        // 获取插件名称
+        $parseClass = explode('\\', $callClass);
+        // 检测是否正确获取到插件名称
+        if (empty($parseClass[1]) || $parseClass[0] != 'plugin') {
+            throw new Exception('插件名称获取失败');
+        }
+        // 返回插件名称
+        return $parseClass[1];
+    }
+}
